7f5076165bc6dc0f4bbf87dcb2289a9e8c0ebe64
tbumgard <tbumgard@gmail.com>
2023-10-12 13:56:32 -0400

    First commit, includes .gitignore, .nvmrc, crawl.js, crawl.test.js, main.js, package-lock.json, and package.json

---
+++ crawl.test.js
@@ -0,0 +1,24 @@
+const { test, expect } = require('@jest/globals')
+const { normalizeURL } = require('./crawl.js')
+
+const url1 = "https://blog.boot.dev/path/"
+const url2 = "https://blog.boot.dev/path"
+const url3 = "http://blog.boot.dev/path/"
+const url4 = "http://blog.boot.dev/path"
+const expectedUrl = "blog.boot.dev/path"
+
+test(`Normalizing ${url1}`, () => {
+    expect(normalizeURL(url1)).toBe(expectedUrl);
+})
+
+test(`Normalizing ${url2}`, () => {
+    expect(normalizeURL(url2)).toBe(expectedUrl);
+})
+
+test(`Normalizing ${url3}`, () => {
+    expect(normalizeURL(url3)).toBe(expectedUrl);
+})
+
+test(`Normalizing ${url4}`, () => {
+    expect(normalizeURL(url4)).toBe(expectedUrl);
+})
